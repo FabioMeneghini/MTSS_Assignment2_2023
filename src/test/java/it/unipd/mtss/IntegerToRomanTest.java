849416ab9f8bfff0ffec937ab0ff499b4485b47a
Lazzarin Nicola <niklazz2002@gmail.com>
2023-05-13 15:17:57 +0200

    close #3

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -10,25 +10,53 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class IntegerToRomanTest {
+	@Test
+    public void testConvertZero() {
+        String zero=IntegerToRoman.convert(0);
+        String expectedZero="";
+        assertEquals(expectedZero,zero);
+    }
     @Test
     public void testConvertOne() {
         String one=IntegerToRoman.convert(1);
         String expectedOne="I";
-        assertEquals(one,expectedOne);
+        assertEquals(expectedOne,one);
     }
 
     @Test
     public void testConvertTwo() {
         String two=IntegerToRoman.convert(2);
         String expectedTwo="II";
-        assertEquals(two,expectedTwo);
+        assertEquals(expectedTwo,two);
     }
 
     @Test
     public void testConvertThree() {
         String three=IntegerToRoman.convert(3);
         String expectedThree="III";
-        assertEquals(three,expectedThree);
+        assertEquals(expectedThree,three);
+    }
+    
+    
+    @Test
+    public void testConvertNegativeOne() {
+        String negativeOne=IntegerToRoman.convert(-1);
+        String expectedNegativeOne="I";
+        assertEquals(expectedNegativeOne,negativeOne);
+    }
+
+    @Test
+    public void testConvertFive() {
+        String five=IntegerToRoman.convert(5);
+        String expectedFive="V";
+        assertEquals(expectedFive,five);
+    }
+
+    @Test
+    public void testConvertSix() {
+        String six=IntegerToRoman.convert(6);
+        String expectedSix="VI";
+        assertEquals(expectedSix,six);
     }
 
 }
